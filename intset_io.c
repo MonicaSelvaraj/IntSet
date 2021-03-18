de4e7cdb6dc3b53dfc41c2709ac04e63ff8bd03d
selvaraj <selvaraj@tak.wi.mit.edu>
2021-03-18 16:01:49 -0400

    Implemented function to remove leading and trailing whitespace

--- intset_io.c
+++ intset_io.c
@@ -4,6 +4,29 @@
 #include <stdint.h>
 PG_MODULE_MAGIC;
 
+/*************************** INTSET TYPE CREATION *****************************/
+
+/* Representation for a set of integers */
+typedef struct {
+        int size;
+        int elems[1];
+}IntSet;
+
+/* Function to create a new set */
+IntSet newSet (int size){
+        IntSet *set;
+        set = malloc(sizeof(IntSet) + sizeof(int)*(size-1));
+        if(set) set->size = size;
+        return set;
+}
+/* use set->elems[0] = 1;.. to store the integers in the set */
+/* you need to keep the array sorted - will make comparing two sets easier */
+/* for the set of sets you can use a hash table/dynamically allocated array */
+
+/*****************************************************************************/
+
+/****************************** PARSING INPUT ********************************/
+
 /* STEPS TO CHECK INPUT VALIDITY
  * First strip off trailing whitespace before first and last character. 
  * The first character should be '{'. 
@@ -24,22 +47,38 @@ PG_MODULE_MAGIC;
 /* TODO: Function to check the validity of a number in the set. 
  * Input is a string.
  * Output is a boolean. 
- */ 
-
-
-/* Representation for a set of integers */
-typedef struct {
-	int size;
-	int elems[1];
-}IntSet;
-
-/* Function to create a new set */
-IntSet newSet (int size){
-	IntSet *set;
-	set = malloc(sizeof(IntSet) + sizeof(int)*(size-1));
-	if(set) set->size = size;
-	return set;
+ */
+bool isValidNum(char* num){
+	//Whitespace should not matter - so strip leading and trailing whitespace
+	
 }
-/* use set->elems[0] = 1;.. to store the integers in the set */
-/* you need to keep the array sorted - will make comparing two sets easier */
-/* for the set of sets you can use a hash table/dynamically allocated array */
+
+/* This function removes leading and trailing whitespace.
+ * Returns a new, trimmed string.
+ * Returns original string if it is just a whitespace character. 
+ * Note: This is adapted from stack overflow. 
+ */
+char* removeWhitespace(char* input){
+	
+	//Pointer for last character of input string 
+	char* last;
+	last = input + strlen(input) - 1; 
+  
+	//Return original string if all whitespaces
+  	if(*input == 0)
+    		return input;
+  
+	// Moving pointer to first non-whitespace character, removes leading whitespace characters of input string
+  	while(isspace((unsigned char)*input)) 
+	  	input++;
+
+  	// Moving last pointer to last non-whitespace character of input string
+	while(last > input && isspace((unsigned char)*last)) last--;
+
+  	// Write new null terminator character
+  	last[1] = '\0';
+
+  return input;
+}
+
+/*****************************************************************************/
